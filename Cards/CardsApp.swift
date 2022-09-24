f66001ac24e92ded4cf55a022967244104650e49
Armin <armin536@gmail.com>
2022-09-24 21:39:48 +0330

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Armin on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
